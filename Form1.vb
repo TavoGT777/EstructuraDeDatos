4b8e3e7c6b0b19bbb7af8f9ea45c37f0856df656
Gustavo Garcia <101522139+TavoGT777@users.noreply.github.com>
2022-03-18 15:12:16 -0600

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim Func As New Funciones
+    Public X2, x As Integer
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        X2 = Func.Factorial(Val(TextBox1.Text))
+        Label2.Text = X2
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        X2 = Func.Elevar(Val(TextBox1.Text), Val(TextBox2.Text))
+        Label2.Text = X2
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        X2 = Func.F(Val(TextBox1.Text))
+        Label2.Text = X2
+    End Sub
+
+End Class
